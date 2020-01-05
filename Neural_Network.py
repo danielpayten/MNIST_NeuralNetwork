38e7e9fcbde5583a65c3704cf1db66f66f5909c8
danielpayten <daniel.payten@gmail.com>
2020-01-05 11:12:21 +0000

    Properly scale pixels of each image Load Labels and pre-process.

--- Neural_Network.py
+++ Neural_Network.py
@@ -2,6 +2,7 @@ import gzip
 import numpy as np
 import requests
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 
 def download_files():
@@ -19,14 +20,45 @@ def download_files():
 def preprocess_images():
     image_dim = 28
     image_cnt = 60000
-    f = gzip.open('train-images-idx3-ubyte.gz','r')
+    f = gzip.open('train-images-idx3-ubyte.gz', 'r')
+    # Skip the first 16 lines (preamble)
     f.read(16)
     data = np.frombuffer(f.read(image_dim * image_dim * image_cnt), dtype=np.uint8).astype(np.float32)
-    data = data.reshape(image_cnt, 28, 28, 1)
+    data = data.reshape(image_cnt, image_dim*image_dim)
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(data)
+    scaled_data = scaled_data.reshape(image_cnt, image_dim, image_dim)
+    return scaled_data
+
+def preprocess_labs():
+    image_cnt = 60000
+    f = gzip.open('train-labels-idx1-ubyte.gz', 'r')
+    # Skip the first 8 lines (preamble)
+    f.read(8)
+    labs = np.frombuffer(f.read(), dtype=np.uint8).astype(np.int)
+    labs = labs.reshape(image_cnt, 1)
+
+    # rather than having labs formatted as a vector of integers, need logical row vector for each
+    # all zeros with a 1 in the relevant digit column.
+    y_mat = np.zeros( (len(labs), 10))
+    integer_array = np.arange(10)
+
+    for i, row in enumerate(labs):
+        logic_matrix = np.asarray(integer_array == row).astype(np.int64)
+        y_mat[i] = logic_matrix
+    return y_mat
 
 
-def display_image(img_data,index):
+def display_image(img_data, index):
     image_ndarray = np.asarray(img_data[index]).squeeze()
     plt.imshow(image_ndarray)
     plt.show()
+
+
+
+
+
+
+
+
 
